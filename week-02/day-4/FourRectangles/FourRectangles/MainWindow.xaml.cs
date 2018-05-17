5790984666082167f35afacead27ff0b982d35cf
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-17 15:41:51 +0200

    chore(FourRectangles)

---
+++ week-02/day-4/FourRectangles/FourRectangles/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace FourRectangles
+{
+
+    public partial class MainWindow : Window
+    // draw four different size and color rectangles.
+    // avoid code duplication.
+
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            Color[] ShapeColor = new Color[] {Colors.Blue, Colors.Green, Colors.Honeydew, Colors.Gray};
+
+                
+            for (int i = 0; i < ShapeColor.Length; i++)
+                {
+                    foxDraw.FillColor(ShapeColor[i]);
+                    foxDraw.DrawRectangle(10 * i, 10 * i, 20 * i, 40 * i);
+                }
+        }
+    }
+
+}

96200fca3f7356c6b8fa2021be2b55d1ba97425e
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-17 14:22:30 +0200

    <type>add(Square)

---
+++ week-02/day-4/CenteredSquare/CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace CenteredSquare
+{
+
+    public partial class MainWindow : Window
+    // Draw a green 10x10 square to the canvas' center.
+
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.DrawRectangle(395, 220, 10, 10);
+        }
+    }
+
+}

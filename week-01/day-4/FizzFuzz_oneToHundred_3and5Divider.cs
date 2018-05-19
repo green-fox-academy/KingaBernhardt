66d5eef14ecc216273f1595fa18e9f841d8b4030
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-19 13:12:05 +0200

    feat(FizzFuzz)

--- week-01/day-4/FizzFuzz_oneToHundred_3and5Divider.cs
+++ week-01/day-4/FizzFuzz_oneToHundred_3and5Divider.cs
@@ -6,25 +6,23 @@ namespace fromOnToHundred_FizzBuzz
     {
         static void Main(string[] args)
         {
-
             for (int i=1; i<100; i++)
             {
-
-                if (i % 3 == 0 && i % 5 == 0)
+                switch (i % 3 == 0 && i % 5 == 0)
                 {
-                    Console.WriteLine("FizzBuzz");
-                } else if (i%3==0)
-                {
+                    case 1: Console.WriteLine("FizzBuzz");
+                        break;
+                    case 2: (i % 3 == 0);
                     Console.WriteLine("Fizz");
-                } else if (i%5 == 0)
-                {
+                        break;
+                    case 3: (i % 5 == 0);
                     Console.WriteLine("Buzz");
-                } else
-                {
+                        break;
+                    default:
                     Console.WriteLine(i);
+                        break;
                 }
             }
-
             Console.ReadLine();
         }
     }

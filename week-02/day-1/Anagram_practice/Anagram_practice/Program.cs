d2d6d0b25bd0f35ac24c66b6ae95da084a947ef9
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-21 20:36:05 +0200

    chore(Anagram)

---
+++ week-02/day-1/Anagram_practice/Anagram_practice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Anagram_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter one word!");
+            string firstGivenWord = Console.ReadLine();
+            Console.WriteLine("Please enter a second word!");
+            string secondGivenWord = Console.ReadLine();
+            bool isAnagramOrNot = Anagram(firstGivenWord, secondGivenWord);
+            Console.WriteLine(isAnagramOrNot);
+            Console.ReadLine();
+        }
+        public static bool Anagram(string word1, string word2)
+        {
+            string createdWord = "";
+            int sameLetter = 0;
+            if (word1.Length != word2.Length)
+            {
+                return false;
+            }
+            else if (word1 == word2)
+            {
+                for (int i = 0; i < word1.Length; i++)
+                {
+                    for (int j = 0; j < word2.Length; j++)
+                    {
+                        if (word1[i] == word2[j])
+                        {
+                            word2 = " " + word2.Remove(j, 1);
+                        }
+                        else if (j == word1.Length - 1)
+                        {
+                            return false;
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return true;
+            }
+            return true;
+        }
+    }
+}

c6e51b3b4236c79674b32a92501be907938c6d0b
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-23 13:59:47 +0200

    chore(Sharpie)

---
+++ week-03/day-3/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sharpie Matey = new Sharpie("white", 0.5f);
+            Sharpie Lola = new Sharpie(100);
+            Matey.Use(11);
+            Lola.Use(21);
+            Console.WriteLine("Lola is " + Lola.color + " it is " + Lola.width + " big, and the ink amount is " + Lola.inkAmount);
+            Console.WriteLine("Matey is " + Matey.color + " it is " + Matey.width + "big and it's ink amount is " + Matey.inkAmount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-03/day-3/Sharpie/Sharpie/Sharpie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpie
+{
+    class Sharpie
+    {
+        public string color;
+        public float width;
+        public float inkAmount;
+
+        public Sharpie(float inkAmount)
+        {
+            color = "Brown";
+            width = 1.7f;
+            this.inkAmount = inkAmount;
+        }
+
+        public Sharpie(string color, float width)
+        {
+            this.color = color;
+            this.width = width;
+            inkAmount = 100;
+        }
+        public float Use(float decreaseAmount)
+        {
+            inkAmount -= decreaseAmount;
+            return inkAmount;
+        }
+    }
+}

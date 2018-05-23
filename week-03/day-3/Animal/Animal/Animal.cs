fdb68f69a6cf456ee7baa8e5fa6659ead0a63a17
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-23 13:01:51 +0200

    chore(Animal)

---
+++ week-03/day-3/Animal/Animal/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class Animal
+    {
+        public int hunger = 50;
+        public int thirst = 50;
+        public int[] thirstAndPlay = { 50, 50 };
+
+        public int Eat()
+        {
+            hunger--;
+            return hunger;
+        }
+        public int Drink()
+        {
+            thirst--;
+            return thirst;
+        }
+        public int[] Play()
+        {
+            for (int i = 0; i < thirstAndPlay.Length; i++)
+            {
+                thirstAndPlay[i]++;
+            }
+            return thirstAndPlay;
+        }
+    }
+}
---
+++ week-03/day-3/Animal/Animal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal camel = new Animal();
+            Console.WriteLine(camel.Drink());
+            Console.WriteLine(camel.Eat());
+            foreach (var item in camel.Play())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}

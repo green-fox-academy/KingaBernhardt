8784071062570b092c2e06f5395b20fc2227a067
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-23 11:29:26 +0200

    chore(PostIt)

---
+++ week-03/day-3/PostIt/PostIt/PostIt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostIt
+{
+    class PostIt
+    {
+        public string backgroundcolor;
+        public string text;
+        public string textColor;
+
+        public PostIt(string backgroundcolor, string text, string textColor)
+        {
+            this.backgroundcolor = backgroundcolor;
+            this.text = text;
+            this.textColor = textColor;
+        }
+    }
+}
---
+++ week-03/day-3/PostIt/PostIt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PostIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt postItFirst = new PostIt("orange", "Idea 1", "blue");
+            PostIt postItSecond = new PostIt("pink", "Awesome", "black");
+            PostIt postItThird = new PostIt("yellow", "Superb", "green");
+            Console.WriteLine("Here is an " + postItFirst.backgroundcolor + " postIt with " + postItFirst.text + " text on it which is " + postItFirst.textColor + ".");
+            Console.ReadLine();
+        }
+    }
+}

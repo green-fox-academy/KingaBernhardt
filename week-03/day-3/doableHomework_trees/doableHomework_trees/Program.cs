cf39c839376fefb795fbff01dc014fbd2707307b
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-23 21:08:43 +0200

    chore(doableHomework_trees)

---
+++ week-03/day-3/doableHomework_trees/doableHomework_trees/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace doableHomework_trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree treeFirst = new Tree("oak", "green", 50, "male");
+            Tree treeSecond = new Tree("willow", "darkgreen", 40, "female");
+            Tree treeThird = new Tree("maple", "orange", 45, "female");
+            Tree treeFourth = new Tree("pine", "brown", 55, "male");
+            Tree treeFiveth = new Tree("birch", "yellow", 20, "female");
+
+            Console.WriteLine("The first tree is an " + treeFirst.name + " it is " + treeFirst.color + " it's age is " + treeFirst.age + " and it is a " + treeFirst.sex + ".");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-03/day-3/doableHomework_trees/doableHomework_trees/Tree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace doableHomework_trees
+{
+    class Tree
+    {
+        public string name;
+        public string color;
+        public int age;
+        public string sex;
+
+        public Tree(string name, string color, int age, string sex)
+        {
+            this.name = name;
+            this.color = color;
+            this.age = age;
+            this.sex = sex;
+        }
+    }
+}

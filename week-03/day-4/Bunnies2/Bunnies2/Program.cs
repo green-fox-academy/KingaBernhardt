05c9dd52c0b4d831ebc9e19f93d6e18489205553
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-24 12:41:58 +0200

    chore(Bunnies2)

---
+++ week-03/day-4/Bunnies2/Bunnies2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bunnies2
+{
+    class Program
+    // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+    // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+    // have 3 ears, because they each have a raised foot. Recursively return the
+    // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter how many bunny you want");
+            int bunnyNums = int.Parse(Console.ReadLine());
+            Console.WriteLine(BunniesTwo(bunnyNums));
+            Console.ReadLine();
+        }
+        public static int BunniesTwo(int bunnyNums)
+        {
+            if (bunnyNums == 0)
+            {
+                return 0;
+            }
+            else if (bunnyNums % 2 == 0)
+            {
+                return 3 + (BunniesTwo(bunnyNums - 1));
+            }
+            else
+            {
+                return 2 + (BunniesTwo(bunnyNums - 1));
+            }
+        }
+    }
+}

d7e4fecd7f52c28e87c0151d43d47b11cac2a268
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-24 11:02:36 +0200

    chore(NumberAdd)

---
+++ week-03/day-4/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumberAdder
+{
+    class Program
+    // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number less than 20.");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(NumberAdder(n));
+            Console.ReadLine();
+        }
+        public static int NumberAdder(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return n + NumberAdder(n - 1);
+            }
+        }
+    }
+}

31da4774bfd7997c5804edd1cb7a3d5c0ee5a28f
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-24 10:47:55 +0200

    chore(Counter)

--- week-03/day-4/Counter/Counter/Program.cs
+++ week-03/day-4/Counter/Counter/Program.cs
@@ -6,7 +6,22 @@ namespace Counter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number less then 20.");
+            int n = int.Parse(Console.ReadLine());
+            CountBack(n);
+            Console.ReadLine();
+        }
+        public static int CountBack(int n)
+        {
+            if (n == 0)
+            {
+               return 0;
+            }
+            else
+            {
+               Console.WriteLine(n);
+               return CountBack(n - 1);
+            }
         }
     }
 }

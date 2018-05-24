6a83f30ca9f369df39e401d1b68ef58d5aa673e8
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-24 14:58:13 +0200

    chore(String3)

---
+++ week-03/day-4/String3/String3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace String3
+{
+    class Program
+    // Given a string, compute recursively a new string where all the
+    // adjacent chars are now separated by a "*".
+    {
+        static void Main(string[] args)
+        {
+            string givenInput = "cool";
+            Console.WriteLine(StringThree(givenInput));
+            Console.ReadLine();
+        }
+        public static string StringThree(string givenInput)
+        {
+            if (givenInput.Length - 1 <= 0 )
+            {
+                return givenInput[0].ToString();
+            }
+            else
+            {
+                return givenInput[0] + "*" + StringThree(givenInput.Remove(0, 1));
+            }
+        }
+    }
+}

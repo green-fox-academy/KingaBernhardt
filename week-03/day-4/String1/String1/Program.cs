89c7d9967177da7a7e180ac0e687dd7e4cb2638f
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-24 12:45:34 +0200

    feat(String1):add"method"

---
+++ week-03/day-4/String1/String1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace String1
+{
+    class Program
+    // Given a string, compute recursively (no loops) a new string where all the
+    // lowercase 'x' chars have been changed to 'y' chars.
+    {
+        static void Main(string[] args)
+        {
+            string givenWord = "xylophone";
+            Console.WriteLine();
+        }
+        public static string StringOne(string givenWord)
+        {
+            if (true)
+            {
+
+            }
+        }
+    }
+}

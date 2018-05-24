681194cd48e764aa4edf45bda5b4a23462c40be4
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-24 11:42:51 +0200

    chore(SumDigit)

---
+++ week-03/day-4/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumDigit
+{
+    class Program
+    // Given a non-negative int n, return the sum of its digits recursively (no loops). 
+    // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+    // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a non-negative number less than 300");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumDigit(n));
+            Console.ReadLine();
+        }
+        public static int SumDigit(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return n % 10 + SumDigit(n / 10);
+            }
+        }
+    }
+}

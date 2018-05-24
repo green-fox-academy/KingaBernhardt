2620b8c70c159978dc9ac07f1e1fabf41b6451e5
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-24 10:24:02 +0200

    chore(Fibonacci)

---
+++ week-03/day-4/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+        //The first two number addintion is the third number.
+        //0, 1, 1, 2, 3, 5, 8, 13, 21
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFibonacci(8));
+            Console.ReadLine();
+        }
+        public static int GetFibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            }
+        }
+    }
+}

82ebaba8547d57810c2c3524b0830d3441301118
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-26 13:06:34 +0200

    chore(Blog)

---
+++ week-03/day-3/Blog/Blog/Blog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog
+{
+    class Blog
+    {
+        List<BlogPost> blogPosts = new List<BlogPost>();
+
+        public void Delete(int givenIndex)
+        {
+            for (int i = givenIndex; i < blogPosts.Count; i++)
+            {
+                blogPosts.RemoveAt(givenIndex);
+            }
+        }
+        public void PrintOutTheBlog()
+        {
+            foreach (var item in blogPosts)
+            {
+                Console.WriteLine($"{item.authorName}, {item.title}, {item.text}, {item.publicationDate}");
+            }
+        }
+        public void Update(int updatePart, BlogPost partToUpdate)
+        {
+            blogPosts[updatePart] = partToUpdate;
+        }
+    }
+}
---
+++ week-03/day-3/Blog/Blog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blog myBlog = new Blog();
+            BlogPost healthyEating = new BlogPost("Kinga Bernhardt", "How to eat healthy", "Vegetables and fruits in a smoothie", "2018.05.26");
+            BlogPost outdoor = new BlogPost("July Ham", "Have fun", "The greatness of being outdoors", "2016.02.21");
+            myBlog.Delete(3);
+            myBlog.Update(4, healthyEating);
+            myBlog.PrintOutTheBlog();
+            Console.ReadLine();
+        }
+    }
+}

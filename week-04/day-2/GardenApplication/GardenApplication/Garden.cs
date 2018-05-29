9354be239b68748d05e7a1ad8ba4361e837c7ffa
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-29 14:29:17 +0200

    chore(GardenApplication):add classes

---
+++ week-04/day-2/GardenApplication/GardenApplication/Garden.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GardenApplication
+{
+    class Garden
+    {
+        List<Plant> plants = new List<Plant>();
+        public List<Plant> Plant
+        {
+            get
+            {
+                return plants;
+            }
+        }
+
+    }
+}
---
+++ week-04/day-2/GardenApplication/GardenApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GardenApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Garden myLittleGarden = new Garden();
+            Flower yellowFlower = new Flower();
+            Flower blueFlower = new Flower();
+            Tree purpleTree = new Tree();
+            Tree orangeTree = new Tree();
+            myLittleGarden.Plant.Add(yellowFlower);
+            myLittleGarden.Plant.Add(blueFlower);
+            myLittleGarden.Plant.Add(purpleTree);
+            myLittleGarden.Plant.Add(orangeTree);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}

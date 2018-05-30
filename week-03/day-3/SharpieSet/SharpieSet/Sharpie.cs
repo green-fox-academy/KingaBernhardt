98b5576f9debf538b7fa6f6726407c8e0e4950be
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-30 06:36:20 +0200

    feat(SharpieSet):add space between Methods

--- week-03/day-3/SharpieSet/SharpieSet/Sharpie.cs
+++ week-03/day-3/SharpieSet/SharpieSet/Sharpie.cs
@@ -23,12 +23,14 @@ namespace SharpieSet
             this.width = width;
             inkAmount = 100;
         }
+
         public Sharpie(string color)
         {
             this.color = color;
             width = 0.4f;
             inkAmount = 7.4f;
         }
+
         public float Use(float decreaseAmount)
         {
             inkAmount -= decreaseAmount;

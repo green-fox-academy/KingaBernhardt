e0b56b08a016ce5a535af67dd3c25d974a09e57e
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-30 14:12:51 +0200

    chore(Comparable)

---
+++ week-04/day-3/CompareTo/Dominoes/domino.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareTo
+{
+    class Domino : IComparable<Domino>
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int CompareTo(Domino domino)
+        {
+            return this.GetValues()[0].CompareTo(domino.GetValues()[0]);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}, {1}]", GetValues()[0],GetValues()[1]);
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+
+    }
+}

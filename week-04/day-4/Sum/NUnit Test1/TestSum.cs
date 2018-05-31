d6ce7c035e1ae7cb06dab09133890c210d964c77
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-31 12:53:23 +0200

    chore(Sum)

---
+++ week-04/day-4/Sum/NUnit Test1/TestSum.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnit Test1
+{
+    [TestFixture]
+    public class TestSum
+    {
+        [TestCase]
+        public void ShouldReturnTheSumOfIntegers()
+        {
+
+        }
+
+    }
+}

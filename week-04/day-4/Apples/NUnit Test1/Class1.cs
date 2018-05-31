7b9f25d76510463764a4c61953880e1e018464a6
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-31 12:48:00 +0200

    chore(Apples)

---
+++ week-04/day-4/Apples/NUnit Test1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Apples;
+
+namespace NUnitTest1
+{
+    [TestFixture]
+    public class TestOutput
+    {
+        [TestCase]
+        public void TestIfReturnOutputSameAsInput()
+        {
+        Assert.AreEqual(Apple.GetApple(), "apple");
+        }
+    }
+}

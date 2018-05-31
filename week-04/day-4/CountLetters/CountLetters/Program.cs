a4b8d53f80dc76a698c7d7136d066b789425bc6f
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-31 16:57:51 +0200

    chore(CountLetters)

---
+++ week-04/day-4/CountLetters/CountLetters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CountLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public Dictionary<string, int> CountLetters(string givenWord)
+        {
+            Dictionary<string, int> setWords = new Dictionary<string, int>();
+            return;
+        }
+    }
+}

2a99e1a4f5e4ba9ac81f784d28cfbdacf6ccc783
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-31 14:20:27 +0200

    feat(Anagram):add method

--- week-04/day-4/Anagram/Anagram/Program.cs
+++ week-04/day-4/Anagram/Anagram/Program.cs
@@ -6,7 +6,38 @@ namespace Anagram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+        }
+
+        public bool GetAnagram(string givenWordOne, string givenWordTwo)
+        {
+            if (givenWordOne.Length != givenWordTwo.Length)
+            {
+                return false;
+            }
+            else if (givenWordOne == givenWordTwo)
+            {
+                for (int i = 0; i < givenWordOne.Length; i++)
+                {
+                    for (int j = 0; j < givenWordTwo.Length; j++)
+                    {
+                        if (givenWordOne[i] == givenWordTwo[j])
+                        {
+                            givenWordTwo = " " + givenWordTwo.Remove(j, 1);
+                        }
+                        else if (j == givenWordOne.Length - 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return true;
+            }
+            return true;
         }
     }
 }

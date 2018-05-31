683f040eb4bf94819f04bf09f3f4b7386317ccfa
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-31 21:02:34 +0200

    feat(FibonacciTesting):add testcases

--- week-04/day-4/FibonacciTesting/FibonacciTest/TestFibonacciTask.cs
+++ week-04/day-4/FibonacciTesting/FibonacciTest/TestFibonacciTask.cs
@@ -11,9 +11,27 @@ namespace FibonacciTesting
         Program fibProgram = new Program();
 
         [TestCase]
-        public void TestFibonacciWithNumber()
+        public void TestFibonacciWithEight()
         {
-            Assert.AreEqual(34, fibProgram.FibonacciForTesting(2));
+            Assert.AreEqual(21, fibProgram.FibonacciForTesting(8));
+        }
+
+        [TestCase]
+        public void TestFibonacciWithTwo()
+        {
+            Assert.AreEqual(4, fibProgram.FibonacciForTesting(2));
+        }
+
+        [TestCase]
+        public void TestFibonacciWithNegative()
+        {
+            Assert.AreEqual(120, fibProgram.FibonacciForTesting(-7));
+        }
+
+        [TestCase]
+        public void TestFibonacciWithZero()
+        {
+            Assert.AreEqual(12, fibProgram.FibonacciForTesting(0));
         }
     }
 }

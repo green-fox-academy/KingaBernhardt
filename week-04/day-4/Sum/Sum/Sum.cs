c01d45ea71bcd660f71de60fbf1495df5c7d69c6
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-31 13:10:25 +0200

    feat(Sum)add a class and method

---
+++ week-04/day-4/Sum/Sum/Sum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sum
+{
+    public class Sum
+    {
+        List<int> numbers = new List<int>() { 4, 6, 5, 5};
+        public int SumNums(List<int> numbers)
+        {
+            int result = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                result += numbers[i];
+            }
+            return result;
+        }
+    }
+}

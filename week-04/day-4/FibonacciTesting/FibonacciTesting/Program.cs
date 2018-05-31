538692a227815e1805789ad2a666e12bc6d772fe
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-05-31 20:31:14 +0200

    chore(FibonacciTesting)

---
+++ week-04/day-4/FibonacciTesting/FibonacciTesting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FibonacciTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int FibonacciForTesting(int numberOne)
+        {
+
+        }
+    }
+}

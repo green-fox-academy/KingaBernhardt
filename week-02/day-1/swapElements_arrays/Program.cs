c66a2fb6e21588a91e28f23451234a6e1891aeea
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-03 10:53:27 +0200

    feat(swapElements_arrays):delete empty lines

--- week-02/day-1/swapElements_arrays/Program.cs
+++ week-02/day-1/swapElements_arrays/Program.cs
@@ -19,11 +19,8 @@ namespace swapElements_arrays
             for (int i = 0; i < abc.Length; i++)
             {
                 Console.WriteLine(abc[i]);
-
             }
             Console.ReadLine();
-
-
         }
     }
 }

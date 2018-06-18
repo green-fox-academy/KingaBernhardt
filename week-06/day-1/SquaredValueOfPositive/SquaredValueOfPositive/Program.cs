ef7e2080d61876260c12c1b1447a79fe34b6daf9
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 11:47:47 +0200

    chore(SquaredValueOfPositive)

---
+++ week-06/day-1/SquaredValueOfPositive/SquaredValueOfPositive/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SquaredValueOfPositive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> posNum = from num in n
+                                      where num > 0
+                                      let squareNum = num * num
+                                      orderby squareNum ascending
+                                      select squareNum;
+            foreach (var item in posNum)
+            {
+                Console.WriteLine("The squared values of the positive numbers: {0}", item );
+            }
+            Console.ReadLine();
+        }
+    }
+}

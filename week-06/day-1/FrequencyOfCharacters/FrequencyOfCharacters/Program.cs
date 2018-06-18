a686d9cb19e89114fd2db6dcc653fa51d44d4731
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 13:25:34 +0200

    chore(StartEndString)

--- week-06/day-1/FrequencyOfCharacters/FrequencyOfCharacters/Program.cs
+++ week-06/day-1/FrequencyOfCharacters/FrequencyOfCharacters/Program.cs
@@ -9,7 +9,11 @@ namespace FrequencyOfCharacters
         //Write a LINQ Expression to find the frequency of characters in a given string!
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Please give me a word");
+            string givenWord = Console.ReadLine();
+
+            var freq = from n in givenWord
+                       where n = n
         }
     }
 }
---
+++ week-06/day-1/StartEndString/StartEndString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StartEndString
+{
+    class Program
+    {
+        //Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+        }
+    }
+}

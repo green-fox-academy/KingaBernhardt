57a07d881cc781e69d8e472e261a7147a77283a6
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 14:34:46 +0200

    feat(FindUppercase):add methods

--- week-06/day-1/FindUppercase/FindUppercase/Program.cs
+++ week-06/day-1/FindUppercase/FindUppercase/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FindUppercase
 {
@@ -7,7 +9,23 @@ namespace FindUppercase
         //Write a LINQ Expression to find the uppercase characters in a string!
         static void Main(string[] args)
         {
-            
+            string word = "haLE";
+            IEnumerable<char> letter = from n in word
+                                       where char.IsUpper(n)
+                                       select n;
+
+            foreach (var item in letter)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            IEnumerable<char> upLetter2 = word.Where(u => char.IsUpper(u));
+            foreach (var item in upLetter2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
     }
 }

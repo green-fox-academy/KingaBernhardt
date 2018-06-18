9f93d229134ab8d7859d5b6de621f9c816b8a240
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 12:45:50 +0200

    feat(FrequencyOfNums):add method syntax

--- week-06/day-1/FrequencyOfNums/FrequencyOfNums/Program.cs
+++ week-06/day-1/FrequencyOfNums/FrequencyOfNums/Program.cs
@@ -10,6 +10,22 @@ namespace FrequencyOfNums
         {
             int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
 
+            var freqNum = from num in n
+                          group num by num into numGroup
+                          select numGroup;
+            foreach (var item in freqNum)
+            {
+                Console.WriteLine("Number " + item.Key + " appears " + item.Count() + " times.");
+            }
+
+            Console.WriteLine();
+
+            var numFreq = n.GroupBy(x => x);
+            foreach (var item in numFreq)
+            {
+                Console.WriteLine("Number " + item.Key + " appears " + item.Count() + " times.");
+            }
+            Console.ReadLine();
         }
     }
 }

ebf939a79b6a5821d0f72e29691436166e20e8f4
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 14:36:59 +0200

    chore(CharToString)

---
+++ week-06/day-1/CharToString/CharToString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CharToString
+{
+    class Program
+    {
+        //Write a LINQ Expression to convert a char array to a string!
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

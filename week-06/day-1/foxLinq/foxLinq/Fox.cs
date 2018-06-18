402dfc6c93b496f0e3b70d6e7fdd9a01a5fa52f2
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 17:08:40 +0200

    feat(foxLinq):add working method

--- week-06/day-1/foxLinq/foxLinq/Fox.cs
+++ week-06/day-1/foxLinq/foxLinq/Fox.cs
@@ -13,5 +13,10 @@ namespace foxLinq
         public string Name { get => name; set => name = value; }
         public string Color { get => color; set => color = value; }
         public string Type { get => type; set => type = value; }
+
+        public override string ToString()
+        {
+            return "The fox what we need: " + Name + " is " + Color +" and it is a "  + Type;
+        }
     }
 }
--- week-06/day-1/foxLinq/foxLinq/Program.cs
+++ week-06/day-1/foxLinq/foxLinq/Program.cs
@@ -14,13 +14,34 @@ namespace foxLinq
         static void Main(string[] args)
         {
             List<Fox> foxes = new List<Fox>()
-            { 
+            {
                 new Fox() { Color = "red", Name = "Palinda", Type = "trigger" },
                 new Fox() { Color = "yellow", Name = "Gando", Type = "fulvipes"},
                 new Fox() { Color = "brown", Name = "Vuk", Type = "forest"},
+                new Fox() { Color = "green", Name = "Dub", Type = "pallida"},
+                new Fox() { Color = "green", Name = "Dub", Type = "minor"}
             };
 
+            var findGreen = from g in foxes
+                            where g.Color.Equals("green")
+                            select g;
 
+            foreach (var i in findGreen)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            var findGreenPallida = from p in foxes
+                                   where p.Color.Equals("green")
+                                   where p.Type.Equals("pallida")
+                                   select p;
+
+            foreach (var item in findGreenPallida)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
     }
 }

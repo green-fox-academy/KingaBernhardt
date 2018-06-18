0bf9c261ff9e27113eb1ca8443d0b36c3fe11c01
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 11:59:06 +0200

    feat(SquaredValueMoreThan):add query syntax

--- week-06/day-1/SquaredValueMoreThan/SquaredValueMoreThan/Program.cs
+++ week-06/day-1/SquaredValueMoreThan/SquaredValueMoreThan/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SquaredValueMoreThan
 {
@@ -7,6 +9,17 @@ namespace SquaredValueMoreThan
         static void Main(string[] args)
         {
             int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var nums = from num in n
+                       let squareNum = num * num
+                       where squareNum > 20
+                       select new { num, squareNum };
+            foreach (var item in nums)
+            {
+                Console.Write("\nLINQ : Find the number and its square of an array which is more than 20 : ");
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
     }
 }

646e56261c94c92b3820b2a1a66ccb069f622e9c
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 12:26:10 +0200

    chore(FrequencyOfNums)

---
+++ week-06/day-1/FrequencyOfNums/FrequencyOfNums/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FrequencyOfNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+        }
+    }
+}
--- week-06/day-1/SquaredValueMoreThan/SquaredValueMoreThan/Program.cs
+++ week-06/day-1/SquaredValueMoreThan/SquaredValueMoreThan/Program.cs
@@ -11,9 +11,8 @@ namespace SquaredValueMoreThan
             int[] n = new[] { 3, 9, 2, 8, 6, 5 };
 
             var nums = from num in n
-                       let squareNum = num * num
-                       where squareNum > 20
-                       select new { num, squareNum };
+                       where num * num > 20
+                       select num;
             foreach (var item in nums)
             {
                 Console.Write("\nLINQ : Find the number and its square of an array which is more than 20 : ");

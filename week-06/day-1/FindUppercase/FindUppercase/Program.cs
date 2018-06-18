aa6f9c2fa4a5a0281ec63188eb826d05ca4cf0eb
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 13:56:48 +0200

    chore(FindUpperCase)

---
+++ week-06/day-1/FindUppercase/FindUppercase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FindUppercase
+{
+    class Program
+    {
+        //Write a LINQ Expression to find the uppercase characters in a string!
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

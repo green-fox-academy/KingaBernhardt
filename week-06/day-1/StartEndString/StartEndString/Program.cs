6aa69d1989e59dfc2e9de10d9efc3d2d9e539590
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 13:53:36 +0200

    feat(StartEndWith):add method syntax

--- week-06/day-1/StartEndString/StartEndString/Program.cs
+++ week-06/day-1/StartEndString/StartEndString/Program.cs
@@ -10,6 +10,23 @@ namespace StartEndString
         static void Main(string[] args)
         {
             string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            IEnumerable<string> city = from c in cities
+                                       where c.StartsWith('A')
+                                       where c.EndsWith('I')
+                                       select c;
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerable<string> city2 = cities.Where(c => c.StartsWith('A')).Where(d => d.EndsWith('I'));
+            foreach (var item in city2)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadLine();
         }
     }
 }

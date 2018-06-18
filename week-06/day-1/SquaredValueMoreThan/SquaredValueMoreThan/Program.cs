dd2ac9ce8bc24a6f9db756a93b1ddded4642768a
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 11:53:06 +0200

    chore(SquareValueMoreThan)

---
+++ week-06/day-1/SquaredValueMoreThan/SquaredValueMoreThan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SquaredValueMoreThan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+        }
+    }
+}

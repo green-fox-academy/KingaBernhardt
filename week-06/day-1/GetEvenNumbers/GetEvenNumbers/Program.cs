e87198d8ee8b1aa43a2ac11be46b08e844e08dbe
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 10:54:53 +0200

    chore(GetEvenNumbers)

---
+++ week-06/day-1/GetEvenNumbers/GetEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            IEnumerable<int> evenNumbers = from num in n
+                                           where num % 2 == 0
+                                           orderby num ascending
+                                           select num;
+            foreach (var item in evenNumbers)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}

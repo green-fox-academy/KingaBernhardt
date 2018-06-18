441b30d5ac33312dc9c75d0277e87d332382d65f
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-18 15:42:37 +0200

    chore(CharToString)

--- week-06/day-1/CharToString/CharToString/Program.cs
+++ week-06/day-1/CharToString/CharToString/Program.cs
@@ -9,7 +9,14 @@ namespace CharToString
         //Write a LINQ Expression to convert a char array to a string!
         static void Main(string[] args)
         {
-            
+            char[] myArray = { 'a', 'g', 'y' };
+
+            /*IEnumerable<string> makeString = from m in myArray
+                                             where char.*/
+
+            var makeString2 = string.Join("", myArray.Select(a => a));
+            Console.WriteLine(makeString2);
+            Console.ReadLine();
         }
     }
 }

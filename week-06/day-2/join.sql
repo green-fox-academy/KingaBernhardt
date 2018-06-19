47d481bf2887b5e7783d61f14c17676e8a687471
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-19 15:31:00 +0200

    feat(join.sql):correct r.mID

--- week-06/day-2/join.sql
+++ week-06/day-2/join.sql
@@ -1,3 +1,3 @@
 SELECT DISTINCT year FROM movie m, rating r ON r.mID = m.mID WHERE r.stars IN (4,5) ORDER BY m.year ASC;
 
-SELECT DISTINCT m.year FROM movie m, rating r WHERE m.mID = r.rID AND r.stars IN (4,5) ORDER BY m.year ASC;
+SELECT DISTINCT m.year FROM movie m, rating r WHERE m.mID = r.mID AND r.stars IN (4,5) ORDER BY m.year ASC;

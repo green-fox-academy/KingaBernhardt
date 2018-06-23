0290b5392b4bb9c53b32ab42b50a72b27d88ca64
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-23 18:44:33 +0200

    add(NUnitTest1)

---
+++ week-06/day-6_Practice/NUnitTest1/NUnit Test1/NthElement.cs
@@ -0,0 +1,40 @@
+﻿using NthElementTest;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NthElementTest
+{
+    public class NthElement
+    {
+        [TestFixture]
+        public class Test
+        {
+            /**
+            * Write your test cases here. You should be able to invoke the method like this:
+            * double result = NthElement.GetAverageForNthElements(numbers, n);
+            */
+            Program prog = new Program();
+
+            [Test]
+            public void GetAverageForNthElements_testCase1()
+            {
+                int[] numbers = new int[] { 2, 4, 10, 34, 3, 16, 3, 21 };
+                int n = 3;
+                double output = Program.GetAverageForNthElements(numbers, n);
+                Assert.AreEqual(13, output);
+            }
+
+            [Test]
+            public void GetAverageForNthElements_testCase2()
+            {
+                int[] numbers = new int[] { 2, 4, 10, 34, 3, 16, 3, 21 };
+                int n = 10;
+                double output = Program.GetAverageForNthElements(numbers, n);
+                Assert.AreEqual(12, output);
+            }
+        }
+    }
+}
+
---
+++ week-06/day-6_Practice/NUnitTest1/NthElementTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NthElementTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 4, 10, 34, 3, 16, 3, 21 };
+            int n = 2;
+
+            double result = GetAverageForNthElements(numbers, n);
+            Console.WriteLine("The average for every {0} element is {1}", n, result);
+            Console.ReadLine();
+        }
+
+        public static double GetAverageForNthElements(int[] numbers, int n)
+        {
+            List<int> nthNums = new List<int>() { };
+            double sumNum = 0;
+
+            for (int i = 0; i < numbers.Length; i += n)
+            {
+                nthNums.Add(numbers[i]);
+
+            }
+            foreach (var item in nthNums)
+            {
+                sumNum += item;
+            }
+            double averageNum = sumNum * 1.0 / nthNums.Count;
+            return averageNum;
+        }
+    }
+}
+

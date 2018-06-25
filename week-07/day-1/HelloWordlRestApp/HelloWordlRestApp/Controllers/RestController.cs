06cc4cbd43de91b3e31b378e845e2fd6b3b6748e
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-25 12:27:07 +0200

    chore(HelloWorldRestApp)

---
+++ week-07/day-1/HelloWordlRestApp/HelloWordlRestApp/Controllers/RestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWordlRestApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWordlRestApp.Controllers
+{
+    [Route("api")]
+    public class RestController : Controller
+    {
+        [Route("api")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greet()
+        {
+            Greeting greeting = new Greeting() { Id = 1, Content = "Hello, Kinga" };
+            return new JsonResult(greeting);
+        }
+    }
+}

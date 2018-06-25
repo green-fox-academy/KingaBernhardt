4117b94b12f11dbb3d3862d5a7c4693cd6cb384c
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-25 17:45:12 +0200

    chore(oneListTask)

---
+++ week-06/day-2/oneListTask.sql
@@ -0,0 +1,5 @@
+~Return all reviewer names and movie names together in a single list, alphabetized. (Sorting by the first name of the reviewer and first word in the title is fine; no need for special processing on last names or removing "The".
+SELECT reviewer.name FROM REVIEWER
+UNION
+SELECT movie.title FROM movie
+

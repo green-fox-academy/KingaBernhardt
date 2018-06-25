a7a655a67d0c70c3fd4c906a82c5a160021b3407
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-25 12:00:26 +0200

    chore(HelloWorld)

---
+++ week-07/day-1/HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Ocelot! This is the default message.");
+        }
+
+        [Route("hello")]
+        public IActionResult Hello(string name)
+        {
+            return Content("Hello World " + name);
+        }
+    }
+}

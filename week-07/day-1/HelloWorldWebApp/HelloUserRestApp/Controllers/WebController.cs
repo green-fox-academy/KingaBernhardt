9ad9cf92d42266ac479ae7880e3b1b63d7a52531
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-25 13:27:03 +0200

    chore(HelloWorldWebApp)

---
+++ week-07/day-1/HelloWorldWebApp/HelloUserRestApp/Controllers/WebController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloUserRestApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloUserRestApp.Controllers
+{
+    [Route("web")]
+    public class WebController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greet()
+        {
+            var greeting = new Greeting()
+            {
+                Id = 1,
+                Content = "World"
+            };
+            return View(greeting);
+        }
+
+    }
+}

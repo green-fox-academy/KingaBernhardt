9d374d2538ff0156ae683e429cd8ec5a46d6e903
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-26 13:28:27 +0200

    feat(Bank Of Simba):add decimal point to cshtml

--- week-07/day-2/BankOfSimba/BankOfSimba/Models/BankAccount.cs
+++ week-07/day-2/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -7,15 +7,17 @@ namespace BankOfSimba.Models
 {
     public class BankAccount
     {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string AnimalType { get; set; }
+        public string Currency { get; set; }
+
         public BankAccount()
         {
             Currency = "Zebra";
         }
 
-        public string Name { get; set; }
-        public double Balance { get; set; }
-        public string AnimalType { get; set; }
-        public string Currency { get; set; }
+        
 
     }
 }

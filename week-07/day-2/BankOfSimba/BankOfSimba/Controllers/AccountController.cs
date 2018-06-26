1032c0e89280e009d80fcbc29cc4facd1e0ccdce
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-26 13:04:03 +0200

    chore(BankOfSimba)

---
+++ week-07/day-2/BankOfSimba/BankOfSimba/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimba.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Simba()
+        {
+            BankAccount bankAccount = new BankAccount()
+            {
+                Name = "Simba",
+                Balance = 2000,
+                AnimalType = "lion"
+            };
+
+            return View(bankAccount);
+        }
+    }
+}
--- week-07/day-2/BankOfSimba/BankOfSimba/Program.cs
+++ week-07/day-2/BankOfSimba/BankOfSimba/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace HelloUserRestApp
+namespace BankOfSimba
 {
     public class Program
     {

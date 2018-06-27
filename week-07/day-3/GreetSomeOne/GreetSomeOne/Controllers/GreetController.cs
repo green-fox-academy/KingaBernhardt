20d717878e935c4d96f5412ee4fa3810b69b733a
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-27 14:48:18 +0200

    feat(GreetSomeOne):add methods and folder

---
+++ week-07/day-3/GreetSomeOne/GreetSomeOne/Controllers/GreetController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreetSomeOne.Controllers
+{
+    public class GreetController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/")]
+        public IActionResult Greeting()
+        {
+            return View();
+        }
+
+        [HttpGet("/")]
+        public IActionResult Greet()
+        {
+            return View();
+        }
+    }
+}

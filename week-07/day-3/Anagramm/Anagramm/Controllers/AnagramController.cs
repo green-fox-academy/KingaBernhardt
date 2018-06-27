749ce7c726b9003e9e0248edad0ff1d53a2ecc61
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-27 16:22:52 +0200

    feat(Anagramm):add view, endpoints

---
+++ week-07/day-3/Anagramm/Anagramm/Controllers/AnagramController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anagramm.Controllers
+{
+    public class AnagramController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/")]
+        public IActionResult PostAnagram(string input)
+        {
+            return View();
+        }
+
+        [HttpGet("/")]
+        public IActionResult GetAnagram()
+        {
+            return View();
+        }
+    }
+}

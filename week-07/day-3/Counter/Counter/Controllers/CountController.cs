25ac3175b543d7013a0499cbe0018bcc9169f383
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-27 11:37:48 +0200

    chore(Counter)

---
+++ week-07/day-3/Counter/Counter/Controllers/CountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Counter.Controllers
+{
+    public class CountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        [Route("Count")]
+        public IActionResult Count()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult RaiseCounter()
+        {
+            if (true)
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ week-07/day-3/Counter/Counter/Models/CountModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Models
+{
+    public class CountModel
+    {
+        public int num { get; set; }
+    }
+}

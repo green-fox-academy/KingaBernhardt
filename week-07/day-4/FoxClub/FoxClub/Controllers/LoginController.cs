375766edfdb077e630d7b00972667ff401cb30f1
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-28 12:59:20 +0200

    feat(FoxClub):do dependency injection in controller

--- week-07/day-4/FoxClub/FoxClub/Controllers/LoginController.cs
+++ week-07/day-4/FoxClub/FoxClub/Controllers/LoginController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoxClub.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoxClub.Controllers
 {
     public class LoginController : Controller
     {
+        private IGiraffeMaker giraffeMaker;
+        public LoginController(IGiraffeMaker giraffeMaker)
+        {
+            this.giraffeMaker = giraffeMaker;
+        }
+
         public IActionResult Index()
         {
             return View();

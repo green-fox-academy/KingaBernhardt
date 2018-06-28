c258a2e7e3e1decf54f304924f90b1e881a72a4b
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-28 12:00:52 +0200

    feat(FoxClub):add controller

---
+++ week-07/day-4/FoxClub/FoxClub/Models/GiraffeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxClub.Models
+{
+    public class GiraffeModel
+    {
+        public string Name { get; set; }
+        public string Food { get; set; }
+        public string Drink { get; set; }
+        public List<TrickModel> TricksOfPets {get; set;}
+    }
+}

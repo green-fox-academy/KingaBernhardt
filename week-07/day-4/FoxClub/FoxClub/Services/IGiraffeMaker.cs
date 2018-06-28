0668b379e2999c35fb0bb7f176980bd58df0b1b4
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-06-28 12:12:49 +0200

    feat(FoxClub):add interface, class

---
+++ week-07/day-4/FoxClub/FoxClub/Services/IGiraffeMaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxClub.Services
+{
+    interface IGiraffeMaker
+    {
+        string Eat();
+        string Drink();
+        string ToString();
+    }
+}

9999c910c1f8af0a05e76d005bc1dd7fbacbd34c
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-07-03 14:39:12 +0200

    feat(ListingTodos):add DI in controller

--- week-08/day-2/ListingTodos/ListingTodos/Controllers/TodoController.cs
+++ week-08/day-2/ListingTodos/ListingTodos/Controllers/TodoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ListingTodos.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ListingTodos.Controllers
@@ -9,6 +10,12 @@ namespace ListingTodos.Controllers
     [Route("/todo")]
     public class TodoController : Controller
     {
+        private TodoRepository todoRepository;
+
+        public TodoController(TodoRepository todoRepository)
+        {
+            this.todoRepository = todoRepository;
+        }
         public IActionResult Index()
         {
             return View();
--- week-08/day-2/ListingTodos/ListingTodos/Startup.cs
+++ week-08/day-2/ListingTodos/ListingTodos/Startup.cs
@@ -21,6 +21,7 @@ namespace ListingTodos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(options => options.UseSqlServer());
             services.AddMvc();
         }
 

09b3196c59550efe3aad8e30ec29c945e13d8577
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-07-05 15:53:40 +0200

    feat(Listingtodos):add ITodoService with methods

---
+++ week-08/day-2/ListingTodos/ListingTodos/Services/ITodoService.cs
@@ -0,0 +1,25 @@
+﻿using ListingTodos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingTodos.Services
+{
+    public interface ITodoService
+    {
+        List<Todo> GetTodoList();
+        void AddTodo(Todo todo);
+        Todo GetTodoId(long id);
+        void RemoveTodo(int id);
+        void EditTodo(Todo todo);
+        List<Todo> GetSearchedTodo(string title);
+
+        List<Assigne> GetAssigneList();
+        void AddAssigne(Assigne assigne);
+        Assigne GetAssigneId(long id);
+        void RemoveAssigne(int id);
+        void EditAssigne(Assigne assigne);
+        List<Assigne> GetSearchedAssigne(string name);
+    }
+}
--- week-08/day-2/ListingTodos/ListingTodos/Startup.cs
+++ week-08/day-2/ListingTodos/ListingTodos/Startup.cs
@@ -27,6 +27,7 @@ namespace ListingTodos
             string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=Todos;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             services.AddDbContext<TodoContext>(options => options.UseSqlServer(connectionString));
             services.AddTransient<TodoRepository>();
+            services.AddTransient<AssigneRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

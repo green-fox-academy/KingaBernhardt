2ade4a7b480284fa7d0830c6e0c0572c5b716331
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-07-06 11:34:07 +0200

    feat(RedditPage):refactor interface

---
+++ week-08/day-5/RedditPage/RedditPage/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using RedditPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditPage.Repositories
+{
+    interface IGenericRepository
+    {
+        void CreatePost(Type element);
+        List<Type> ReadPost();
+        List<Type> SearchPost(Type element);
+        void DeletePost(Type element);
+        void Update(Type element);
+    }
+}

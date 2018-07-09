4bfa86652e069d195b09824377fe6b640ea498a3
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-07-09 16:54:35 +0200

    feat(Frontend):add LogContext

---
+++ week-09/day-1/Frontend/Frontend/Database/LogContext.cs
@@ -0,0 +1,18 @@
+﻿using Frontend.Models.LogEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Database
+{
+    public class LogContext : DbContext
+    {
+        public DbSet<Log> logs { get; set; }
+        public LogContext(DbContextOptions<LogContext> options) : base(options)
+        {
+
+        }
+    }
+}

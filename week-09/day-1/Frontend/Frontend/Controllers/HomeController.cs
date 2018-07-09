110bd94defab89689064a72cc559d846495cad4b
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-07-09 17:20:20 +0200

    feat(Frontend):add log entities into endpoints

--- week-09/day-1/Frontend/Frontend/Controllers/HomeController.cs
+++ week-09/day-1/Frontend/Frontend/Controllers/HomeController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Frontend.Database;
 using Frontend.Models.ArrayModel;
 using Frontend.Models.DoUntil;
+using Frontend.Models.LogEntities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Frontend.Controllers
 {
     public class HomeController : Controller
     {
+        private LogContext logCont;
+        public HomeController(LogContext logCont)
+        {
+            this.logCont = logCont;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -19,6 +27,15 @@ namespace Frontend.Controllers
         [HttpGet("/Doubling")]
         public IActionResult Doubling(int? input)
         {
+            var log = new Log
+            {
+                CreatedAt = DateTime.Now,
+                EndPoint = "/doubling",
+                Data = $"input={input}"
+            };
+            logCont.logs.Add(log);
+            logCont.SaveChanges();
+
             if (input == null)
             {
                 return new JsonResult(new
@@ -39,6 +56,15 @@ namespace Frontend.Controllers
         [HttpGet("/Greeter")]
         public IActionResult Greeter(string name, string title)
         {
+            var log = new Log
+            {
+                CreatedAt = DateTime.Now,
+                EndPoint = "/Greeter",
+                Data = $"name={name}&title={title}"
+            };
+            logCont.logs.Add(log);
+            logCont.SaveChanges();
+
             if (name == null && title == null)
             {
                 return new JsonResult(
@@ -72,6 +98,15 @@ namespace Frontend.Controllers
         [HttpGet("/Appenda/{appendable}")]
         public IActionResult Appenda(string appendable)
         {
+            var log = new Log
+            {
+                CreatedAt = DateTime.Now,
+                EndPoint = $"/appenda/{appendable}",
+                Data = $"appendable={appendable}"
+            };
+            logCont.logs.Add(log);
+            logCont.SaveChanges();
+
             return new JsonResult(new
             {
                 appended = appendable + "a"
@@ -81,28 +116,37 @@ namespace Frontend.Controllers
         [HttpPost("/dountil/{what}")]
         public IActionResult Dountil(string what, [FromBody] Dountil until)
         {
-            if (what == "sum" )
+            var log = new Log
+            {
+                CreatedAt = DateTime.Now,
+                EndPoint = $"/dountil/{what}",
+                Data = $"what={what}&until={until.Until}"
+            };
+            logCont.logs.Add(log);
+            logCont.SaveChanges();
+
+            if (what == "sum")
             {
                 int result = 0;
                 for (int i = 0; i < until.Until; i++)
                 {
                     result += i;
                 }
-                
+
                 return new JsonResult(new
                 {
                     result
                 });
             }
-            else if (what =="factor")
+            else if (what == "factor")
             {
                 int result = 1;
                 for (int i = 1; i <= until.Until; i++)
                 {
                     result *= i;
                 }
-                
-                return new JsonResult (new
+
+                return new JsonResult(new
                 {
                     result
                 });
@@ -119,6 +163,15 @@ namespace Frontend.Controllers
         [HttpPost("/arrays")]
         public IActionResult Arrays([FromBody] Arrays array)
         {
+            var log = new Log
+            {
+                CreatedAt = DateTime.Now,
+                EndPoint = "/arrays",
+                Data = $"input={array.ToString()}"
+            };
+            logCont.logs.Add(log);
+            logCont.SaveChanges();
+
             if (array.What == "sum")
             {
                 int result = 0;
@@ -160,6 +213,17 @@ namespace Frontend.Controllers
                     error = "Please provide numbers."
                 });
             }
+        }
+
+        [HttpGet("/log")]
+        public IActionResult Log()
+        {
+            var response = new LogResponse
+            {
+                logs = logCont.logs.ToList(),
+                EntryCount = logCont.logs.ToList().Count()
+            };
+            return Json(response);
         }
     }
 }

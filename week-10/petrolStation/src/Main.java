1c9f018e8384a3f0b05a5f4c3c5be480a7754e9e
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-08-01 13:58:16 +0200

    feat(petrolStation):add working methods to classes

--- week-10/petrolStation/src/Main.java
+++ week-10/petrolStation/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Car audi = new Car();
+        Car bmw = new Car(10, 40);
+        Station mol = new Station();
+        System.out.println(audi.toString());
+        System.out.println(bmw.toString());
+        System.out.println(mol.toString());
+
+        mol.refill(audi);
+        mol.refill(bmw);
+        System.out.println(audi.toString());
+        System.out.println(bmw.toString());
+        System.out.println(mol.toString());
     }
 }
--- week-10/petrolStation/src/Station.java
+++ week-10/petrolStation/src/Station.java
@@ -1,2 +1,31 @@
 public class Station {
+    private int gasAmount;
+
+    public Station(){
+        gasAmount = 100;
+    }
+
+    public Station(int gasAmount) {
+        this.gasAmount = gasAmount;
+    }
+
+    public void refill(Car car){
+            int neededGas = car.getCapacity() - car.getGasAmount();
+            if (neededGas > 0) {
+                if (gasAmount >= neededGas){
+                    car.setGasAmount(car.getCapacity());
+                    gasAmount -= neededGas;
+                } else {
+                    car.setGasAmount(car.getGasAmount() + gasAmount);
+                    gasAmount = 0;
+                }
+            }
+    }
+
+    @Override
+    public String toString() {
+        return "Station{" +
+                "gasAmount=" + gasAmount +
+                '}';
+    }
 }

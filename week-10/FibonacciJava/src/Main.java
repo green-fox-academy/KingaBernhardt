583c925e4346fa26dd0265dce5dbc4a2b4a159f1
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-08-01 16:33:56 +0200

    add(fibonacciJava)

---
+++ week-10/FibonacciJava/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(fibonacciMaker(8));
+    }
+    public static int fibonacciMaker(int n){
+        if (n == 0){
+            return 0;
+        } else if (n == 1){
+            return 1;
+        } else {
+            return fibonacciMaker(n - 1) + fibonacciMaker((n - 2));
+        }
+    }
+}

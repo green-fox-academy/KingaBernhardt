ff4f396439ecca066fb57a35486020e2b4b27b57
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-08-01 16:56:53 +0200

    add(Fizzbuzz)

---
+++ week-10/fizzBuzz/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        fizzBuzzGame();
+    }
+    public static void fizzBuzzGame(){
+        String output = "";
+        for (int i = 1; i <= 100; i++) {
+            if (i % (5 * 3) == 0) {
+                output = "FizzBuzz";
+            } else if (i % 5 == 0) {
+                output = "Buzz";
+            } else if (i % 3 == 0) {
+                output = "Fizz";
+            } else {
+                output = Integer.toString(i);
+            }
+            System.out.println(output);
+        }
+    }
+}

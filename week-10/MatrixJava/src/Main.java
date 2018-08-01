524da3276c8bc6ac379889dd7c54bab43c18d1d9
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-08-01 12:07:06 +0200

    add(matrixJava)

---
+++ week-10/MatrixJava/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        matrixMaker();
+    }
+
+    public static int[][] matrixMaker(){
+
+        int[][] matrixCreated = new int[4][4];
+        for (int i=0; i < matrixCreated.length; i++){
+            for(int j = 0; j < matrixCreated.length; j++){
+                if (i == j){
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+        return matrixCreated;
+    }
+}

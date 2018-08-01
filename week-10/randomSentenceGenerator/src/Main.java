85ca09d3973852ce8536ace1099c3c85fda84dda
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-08-01 15:29:48 +0200

    add(randomSentenceGenerator)

---
+++ week-10/randomSentenceGenerator/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random generatorClass = new Random();
+        System.out.println(generatorClass.generator());
+    }
+}

88524caf1f50e5853a49188a61d0845024440147
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-09-14 15:17:13 +0200

    feat(Kindergarten):add annotation to child model

--- week-10/Kindergarten/src/main/java/com/kindergarten/demo/Models/Child.java
+++ week-10/Kindergarten/src/main/java/com/kindergarten/demo/Models/Child.java
@@ -1,10 +1,18 @@
 package com.kindergarten.demo.Models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
+import javax.validation.constraints.NegativeOrZero;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Child {
 
   @Id
@@ -16,61 +24,10 @@ public class Child {
 
   @ManyToMany
   private List<Teacher> teachers = new ArrayList<>();
-
-  public Child() {
-  }
-
+  
   public Child(String name, Integer age, String sign) {
     this.name = name;
     this.age = age;
     this.sign = sign;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
-  public void setAge(Integer age) {
-    this.age = age;
-  }
-
-  public String getSign() {
-    return sign;
-  }
-
-  public void setSign(String sign) {
-    this.sign = sign;
-  }
-
-  public Parent getParent() {
-    return parent;
-  }
-
-  public void setParent(Parent parent) {
-    this.parent = parent;
-  }
-
-  public List<Teacher> getTeachers() {
-    return teachers;
-  }
-
-  public void setTeachers(List<Teacher> teachers) {
-    this.teachers = teachers;
   }
 }

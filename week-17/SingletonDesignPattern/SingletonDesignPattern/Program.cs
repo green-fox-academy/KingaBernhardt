36e2f83e042a7266741a02f860387954cffa08a0
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-09-20 16:20:31 +0200

    feat(SingletonDesignPattern):add example pattern

---
+++ week-17/SingletonDesignPattern/SingletonDesignPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PringDetails("This is message from employee");
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PringDetails("This is message from student.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-17/SingletonDesignPattern/SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPattern
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value" + counter.ToString());
+        }
+        public void PringDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
